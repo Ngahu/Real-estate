054a33c031dcf4de0d4d88a5952113d013ae05db
joel <joel@localhost.localdomain>
2016-11-10 01:22:46 +0300

    adding of the js logic that controlls the scrolling

--- mysite/App1/static/js/base.js
+++ mysite/App1/static/js/base.js
@@ -1,3 +1,11 @@
 /**
  * Created by joel on 11/10/16.
  */
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 50 ) {
+        $('nav').addClass('shrink');
+    }
+    else {
+        $('nav').removeClass('shrink');
+    }
+});
