c0f03583be0f99bcee6830400053907ae4473b69
joel <joel@localhost.localdomain>
2016-11-10 02:35:49 +0300

    creating of the top bar

--- mysite/App1/views.py
+++ mysite/App1/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from django import template
+from django.template import loader
 
-# Create your views here.
+def index(request):
+    """the index"""
+    template = loader.get_template('App1/index.html')
+    return HttpResponse(template.render(request))
